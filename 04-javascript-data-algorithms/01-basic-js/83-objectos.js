905720a640ce5bafe74abf769857fba72631adba
Juan Camilo Losada Bultaif <92903961+jclosadab1996@users.noreply.github.com>
2025-06-22 02:49:16 -0500

    fix: corregir errores en la lógica de las funciones de operaciones matemáticas avanzadas
    Se realizan ajustes adicionales en la lógica de las funciones de operaciones matemáticas avanzadas para mejorar su rendimiento y asegurar la precisión en los cálculos complejos.

---
+++ 04-javascript-data-algorithms/01-basic-js/83-objectos.js
@@ -0,0 +1,9 @@
+const myDog = {
+  // Cambia solo el código debajo de esta línea
+  name: "Whiskers",
+  legs: 4,
+  tails: 1,
+  friends: ["Water", "Dogs"],
+
+  // Cambia solo el código encima de esta línea
+};
