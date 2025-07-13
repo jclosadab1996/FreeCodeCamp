629fd2c7e8a32f1995011bec79c80bd83f73f8ad
Juan Camilo Losada Bultaif <92903961+jclosadab1996@users.noreply.github.com>
2025-07-12 22:29:12 -0500

    fix: refinar y optimizar la lógica de las funciones de operaciones matemáticas avanzadas
    Se realizan ajustes y mejoras en la lógica de las funciones de operaciones matemáticas avanzadas para mejorar su rendimiento y asegurar la precisión en cálculos complejos.

---
+++ 04-javascript-data-algorithms/05-Estructura-de-datos-basicas/06-agregar-splice.js
@@ -0,0 +1,16 @@
+function htmlColorNames(arr) {
+  // Cambia solo el código debajo de esta línea
+  arr.splice(0, 2, "DarkSalmon", "BlanchedAlmond");
+  // Cambia solo el código encima de esta línea
+  return arr;
+}
+
+console.log(
+  htmlColorNames([
+    "DarkGoldenRod",
+    "WhiteSmoke",
+    "LavenderBlush",
+    "PaleTurquoise",
+    "FireBrick",
+  ])
+);
---
+++ 04-javascript-data-algorithms/05-Estructura-de-datos-basicas/10-indexOf.js
@@ -0,0 +1,10 @@
+function quickCheck(arr, elem) {
+  // Cambia solo el código debajo de esta línea
+  if (arr.indexOf(elem) >= 0) {
+    return true;
+  }
+  return false;
+  // Cambia solo el código encima de esta línea
+}
+
+console.log(quickCheck(["squash", "onions", "shallots"], "mushrooms"));
