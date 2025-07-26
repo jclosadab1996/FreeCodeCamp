56ae6ebbed5a4c0224e3d7b8f5a6291bcaf8feb3
Juan Camilo Losada Bultaif <92903961+jclosadab1996@users.noreply.github.com>
2025-07-26 01:41:59 -0500

    feat(functional-programming): add functional programming exercises
    Implement various functional programming exercises including:
    - String manipulation with split and join
    - Array sorting and non-mutating operations
    - Filtering and mapping arrays
    - Data analysis using reduce

---
+++ 04-javascript-data-algorithms/08-programacion-funcional/20-join.js
@@ -0,0 +1,8 @@
+function sentensify(str) {
+  // Cambia solo el código debajo de esta línea
+  return str.split(/\W/).join(" ");
+
+  // Cambia solo el código encima de esta línea
+}
+
+sentensify("May-the-force-be-with-you");
